74a77fd0555049c63c879cae64f74163271544a7
Sakshi Chede <78843328+Sakiii8499@users.noreply.github.com>
2021-10-04 20:00:37 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()+[]{}\|/><-=";
+let password = document.getElementById("password");
+
+function generate() {
+  let chars =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&*()";
+  let passwordLength = 12;
+  let password = "";
+  for (let i = 0; i <= passwordLength; i++) {
+    let randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber + 1);
+  }
+  document.getElementById("password").value = password;
+}
 
 function copy() {
-    var copyText = document.getElementsByClassName("password");
-    copyText.select();
-    document.execCommand("copy");  
-  }
+  let copyText = document.getElementById("password");
+  copyText.select();
+  document.exeCommand("copy");
+}
